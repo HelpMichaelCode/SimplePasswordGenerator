4fd02130bacfba99f90989db82150c9e5deaa0d6
Michael <37000338+HelpMichaelCode@users.noreply.github.com>
2026-05-26 14:00:37 +0100

    Added script comments
    Added script comments

--- passwordG.py
+++ passwordG.py
@@ -1,7 +1,10 @@
+# Importing packages to generate random passwords
 import random
 import string
 
+
 def complexPassword():
+    # The complex password will include lowercase letters, digits, punctuation, and at least one uppercase letter.
     characters = string.ascii_lowercase + string.digits + string.punctuation
     password = [random.choice(characters) for _ in range(11)]
 
@@ -13,6 +16,7 @@ def complexPassword():
     print("Generated Complex Password:", password)
 
 def simplePassword():
+    # The simple password will include lowercase letters and digits, and at least one uppercase letter.
     characters = string.ascii_lowercase + string.digits
     password =  [random.choice(characters) for _ in range(11)]
 
@@ -23,11 +27,11 @@ def simplePassword():
 
     print("Generated Simple Password:", password)
 
-
+# Main program
 print("Welcome to the password generator!\nPlease select an option: \n[1] Complex Password\n[2] Simple Password")
 choice = input("Enter your choice: ")
 
-
+# Validate user input
 while choice not in ['1', '2']:
     print("Invalid choice. Please select either [1] or [2].")
     choice = input("Enter your choice: ")
